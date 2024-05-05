1416abaca68d4b5a7c6e1d2fff61b293580e9fe1
Shalini Singh <134768404+Shalini5647@users.noreply.github.com>
2024-05-06 00:12:17 +0530

    Add files via upload

---
+++ month.java
@@ -0,0 +1,50 @@
+import java.util.Scanner;
+
+public class month {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("ENTER THE MONTH: ");
+        String month = sc.nextLine();
+        switch (month) {
+            case "1":
+            System.out.println("JANUARY");
+            break;
+            case "2":
+            System.out.println("FEBRUARY");
+            break;
+            case "3":
+            System.out.println("MARCH");
+            break;
+            case "4":
+            System.out.println("APRIL");
+            break;
+            case "5":
+            System.out.println("MAY");
+            break;
+            case "6":
+            System.out.println("JUNE");
+            break;
+            case "7":
+            System.out.println("JULY");
+            break;
+            case "8":
+            System.out.println("AUGUST");
+            break;
+            case "9":
+            System.out.println("OCTOBER");
+            break;
+            case "10":
+            System.out.println("SEPTEMBER");
+            break;
+            case "11":
+            System.out.println("NOVEMBER");
+            break;
+            case "12":
+            System.out.println("DECEMBER");
+            break;
+            default:
+            System.out.println("INVALID MONTH");
+            break;
+        }
+    }
+}
